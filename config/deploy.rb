016db58125fefa6990e449b44cc5193f0d5b1482
Ignacio Tolosa <ignacio.tolosa.12@sansano.usm.cl>
2015-05-09 20:19:34 -0300

    secret change

--- config/deploy.rb
+++ config/deploy.rb
@@ -44,7 +44,7 @@ set :rbenv_roles, :all # default value
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
